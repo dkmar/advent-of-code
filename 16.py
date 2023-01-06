f523ee645d85cb161721e8a5d539effd81ffa454
Daniel Mar <dkmar@protonmail.com>
2023-01-06 00:39:29 -0800

    completed part2

--- 16.py
+++ 16.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-from typing import FrozenSet
 import util
 import sys, math
-from collections import defaultdict, deque
-from functools import cache
+from collections import defaultdict
+from itertools import combinations
 
 # 18:13
 
@@ -35,27 +34,72 @@ Valve II has flow rate=0; tunnels lead to valves AA, JJ
 Valve JJ has flow rate=21; tunnel leads to valve II
 '''
 
-@cache
-def max_flow(u: str, opened_valves: FrozenSet[str], time: int = 30) -> int:
+def find_distances():
+    # initializing
+    eta = { 
+            u: { v: sys.maxsize for v in graph } 
+            for u in graph 
+          }
+
+    for u,vs in graph.items():
+        eta[u][u] = 0
+        for v in vs:
+            eta[u][v] = 1
+
+    # floyd warshall
+    for v in graph:
+        for u in graph:
+            for w in graph:
+                uv, vw, uw = eta[u][v], eta[v][w], eta[u][w]
+                if uv + vw < uw:
+                    eta[u][w] = uv + vw
+
+    return eta
+
+def possible_paths(u, valves, time, path):
+    for v in valves:
+        new_time = time - (eta[u][v] + 1)
+        if 0 < new_time < 3:
+            yield path + ((new_time, v),)
+        elif new_time >= 3:
+            yield from possible_paths(v, valves - {v}, new_time, path + ((new_time, v),))
+
+    yield path
+
+def score(path):
+    res = 0
+    for (time, valve) in path:
+        res += time * flow_rate[valve]
+    return res
+
+eta = find_distances()
+valves = frozenset(u for u in graph if flow_rate[u] != 0)
+
+# part 1
+paths = possible_paths('AA', valves, 30, ())
+best = max(map(score, paths))
+print(best)
+
+# part 2
+def max_scores(paths):
     '''
-    Return the max possible flow from the path starting at u.
+    find the max score for each set of valves.
     '''
-    if time <= 0:
-        return 0
+    scores = defaultdict(int)
+    for path in paths:
+        opened_valves = frozenset(valve for (_, valve) in path)
+        scores[opened_valves] = max(scores[opened_valves], score(path))
 
-    # case 1: we don't open this valve.
-    best1 = max(max_flow(v, opened_valves, time - 1) for v in graph[u])
+    return scores
 
-    if u in opened_valves or flow_rate[u] == 0:
-        # early exit if it doesn't make sense to open this valve.
-        return best1
+paths = possible_paths('AA', valves, 26, ())
+valveset_score = max_scores(paths)
 
-    # case 2: we open this valve, then transition.
-    score = flow_rate[u] * (time - 1)
-    new_opened_valves = opened_valves | {u}
-    best2 = score + max(max_flow(v, new_opened_valves, time - 2) for v in graph[u])
-    
-    return max(best1,best2)
-
-best = max_flow('AA', frozenset())
+# if there's no overlap between valves opened by me and valves opened by the elephant,
+# we have a candidate. Find the max such candidate.
+best = max(
+    valveset_score[valves1] + valveset_score[valves2]
+    for valves1, valves2 in combinations(valveset_score, 2)
+    if valves1.isdisjoint(valves2)
+)
 print(best)
