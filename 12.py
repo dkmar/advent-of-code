876e5bb5b6654d15c92075ef7ea051a56da55c94
Daniel Mar <dkmar@protonmail.com>
2023-12-13 00:33:44 -0800

    days 12 and 13

---
+++ 12.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+import util
+import sys, math, re, functools, operator, itertools, bisect
+import numpy as np
+from collections import defaultdict, deque, Counter
+from itertools import accumulate, chain, pairwise, cycle, product, combinations, groupby, repeat
+from more_itertools import sliding_window
+from functools import cache, reduce
+
+# 14:53
+infile = sys.argv[1] if len(sys.argv)>1 else '12.in'
+f = open(infile, 'r') if infile != '-' else sys.stdin
+
+lines = list(map(util.Input, f))
+#data = util.Input(f.read())
+#grid = util.Grid.from_text(data)
+
+'''
+??.?#??#?#??##???? 2,4,6,1
+?????#?..?#? 2,2,2
+???#???#.?#?????.# 5,1,1,1,2,1
+???#???#.#??#??##?? 1,1,1,1,9
+????###??.????#.# 5,1,2,1
+
+? = .|#
+want: the sum of possible configs across all given (springs, broken)
+
+????.#####??.#####. 1,6,5
+???? #####??  1,6
+cases:
+1. we meet the first broken
+2. we don't and we leave it for the next group
+??#? #####??  3,6
+need = broken[0] - group.count(#) 
+
+??#? 1,2
+?? # ?
+
+??#??? 3 2
+'''
+
+@cache
+def waysV2(springs: str, broken: tuple[int, ...], group_size: int = 0) -> int:
+    if not springs:
+        return (not broken and group_size == 0)
+    if not broken:
+        return ('#' not in springs)
+
+    ways = 0
+    if springs[0] in '#?':
+        ways += waysV2(springs[1:], broken, group_size + 1)
+    if springs[0] in '.?':
+        # starting a group or terminating a group
+        if group_size == 0:
+            ways += waysV2(springs[1:], broken, 0)
+        elif group_size == broken[0]:
+            ways += waysV2(springs[1:], broken[1:],0)
+
+    return ways
+
+
+@cache
+def countWays(springs: str, broken: tuple[int, ...]) -> int:
+    '''
+    Match the first broken group.
+    ??#?.######..#####. 2,6,5
+    '''
+    if not broken:
+        return int('#' not in springs)
+
+    need = broken[0]
+    ways = 0
+    # window must use the first broken streak if there is one.
+    first_broken = i if (i := springs.find('#')) != -1 else len(springs)
+    for i, window in enumerate(sliding_window(springs, need + 1)):
+        if i > first_broken:
+            break
+        if (window[-1] in '.?') and all(ch in '#?' for ch in window[:need]):
+            ways += countWays(springs[i + need + 1:], broken[1:])
+
+    return ways
+
+
+part1 = part2 = 0
+for line in lines:
+    springs, broken = line.split()
+    broken = tuple(util.get_ints(broken))
+    part1 += waysV2(springs + '.', broken)
+
+    springs = '?'.join(repeat(springs, 5)) + '.'
+    broken = broken * 5
+    part2 += waysV2(springs, broken)
+
+print('Part 1:', part1)
+print('Part 2:', part2)
+print(waysV2.cache_info())
---
+++ 13.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import util
+import sys, math, re, functools, operator, itertools, bisect
+import numpy as np
+from collections import defaultdict, deque, Counter
+from itertools import accumulate, chain, pairwise, cycle, product, combinations
+from functools import cache, reduce
+
+# 21:00
+infile = sys.argv[1] if len(sys.argv)>1 else '13.in'
+f = open(infile, 'r') if infile != '-' else sys.stdin
+
+# lines = list(map(util.Input, f))
+data = util.Input(f.read())
+#grid = util.Grid.from_text(data)
+blocks = data.split('\n\n')
+patterns = [pat.splitlines() for pat in blocks]
+'''
+...##.#
+.##.###
+.##.###
+...##.#
+#...###
+'''
+
+def difference(s1: str, s2: str) -> int:
+    return sum(int(a!=b) for a, b in zip(s1, s2))
+
+def get_reflection_n(pattern: list[str], target_diff: int) -> int:
+    line_numbers = range(len(pattern))
+    for i,j in pairwise(line_numbers):
+        start = i
+        diff = 0
+        while i >= 0 and j < len(pattern):
+            diff += difference(pattern[i], pattern[j])
+            if diff > 1:
+                break
+            i -= 1; j += 1
+        else:
+            if diff == target_diff:
+                return start + 1
+
+    return 0
+
+def answer(pattern: list[str], target_diff: int):
+    # horizontal reflection
+    h = get_reflection_n(pattern, target_diff)
+    if h:
+        return 100 * h
+    # vertical reflection
+    v = get_reflection_n([str(row) for row in zip(*pattern)], target_diff)
+    return v
+
+part1 = part2 = 0
+for pattern in patterns:
+    part1 += answer(pattern, target_diff=0)
+    part2 += answer(pattern, target_diff=1)
+
+print(f'Part 1: {part1}')
+print(f'Part 2: {part2}')
